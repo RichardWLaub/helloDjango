3bf53002d21604a9a0f8fe81bf06318639b05361
Ubuntu <ubuntu@ip-10-0-0-175.ec2.internal>
2017-11-22 09:19:48 +0000

    cleanup channels demo

--- hostname/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HostnameConfig(AppConfig):
-    name = 'hostname'
--- hostname/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import socket
-
-def index(request):
-    return HttpResponse("Hello from {}".format(socket.gethostname()))
