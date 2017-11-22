94053b155206f34493dc889a8ac819f3be008f2d
Ubuntu <ubuntu@ip-10-0-0-175.ec2.internal>
2017-11-22 09:32:12 +0000

    update endpoint

--- realtime/routing.py
+++ realtime/routing.py
@@ -3,9 +3,5 @@ from channels.routing import route
 from .consumers import websocket_receive
 
 channel_routing = [
-<<<<<<< HEAD
     route("websocket.receive", websocket_receive, path=r"^/streams/"),
-=======
-    route("websocket.receive", websocket_receive, path=r"^/chat/"),
->>>>>>> 97623a2bc447b2c27a29f4ffcf4da249df41f661
 ]
